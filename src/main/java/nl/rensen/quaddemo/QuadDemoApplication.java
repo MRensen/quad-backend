0a8089e4b70dd20dc305ded8ac24a4dfca969821
Mark Rensen <markrensen@hotmail.com>
2026-04-23 22:22:52 +0200

    init

---
+++ src/main/java/nl/rensen/quaddemo/QuadDemoApplication.java
@@ -0,0 +1,13 @@
+package nl.rensen.quaddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuadDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuadDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/rensen/quaddemo/QuadDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.rensen.quaddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuadDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
