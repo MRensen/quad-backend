258854536460775885f993e10100774e557123ff
MRensen <markrensen@hotmail.com>
2026-04-25 23:12:07 +0200

    cors

--- src/main/java/nl/rensen/quaddemo/config/CorsConfig.java
+++ src/main/java/nl/rensen/quaddemo/config/CorsConfig.java
@@ -1,15 +1,9 @@
-package nl.rensen.quaddemo;
+package nl.rensen.quaddemo.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.List;
 
 @Configuration
 @EnableWebMvc
@@ -17,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:5173");
+        registry.addMapping("/**").allowedOrigins("http://localhost:5173", "https://trivia-frontend-vtg86.ondigitalocean.app/");
     }
 //    Deze werkt alleen met Spring Security
 //    @Bean
